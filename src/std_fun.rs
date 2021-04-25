45fdab3190cfa078fb36c80e6b6dc08f6ed1c665
hzqd <hzqelf@yeah.net>
2021-04-25 18:28:09 +0800

    optimized

--- src/std_fun.rs
+++ src/std_fun.rs
@@ -1,12 +1,12 @@
 use std::io::stdin;
 use super::std_ext::KtStd;
 
+pub fn read_line() -> String {
+    String::new().also_mut(|s| stdin().read_line(s))
+}
+
 pub fn wait_enter() {
-    loop {
-        if String::new() == String::new().also_mut(|s| stdin().read_line(s)).trim() {
-            break
-        }
-    }
+    while read_line().trim() != "" {}
 }
 
 pub fn y<T, R>(f: impl Copy + Fn(&dyn Fn(T) -> R, T) -> R) -> impl Fn(T) -> R {
