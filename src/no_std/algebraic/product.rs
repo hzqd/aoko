0404ab828fb876b4bb2d267f13c2b6457bfdd3da
hzqd <hzqelf@yeah.net>
2023-12-29 14:28:43 +0800

    Fix cannot-initialize_private-fields error

--- src/no_std/algebraic/product.rs
+++ src/no_std/algebraic/product.rs
@@ -2,7 +2,7 @@ use core::fmt;
 use std::error::Error;
 
 #[derive(Debug)]
-pub struct StrErr<'a>(&'a str);
+pub struct StrErr<'a>(pub &'a str);
 impl Error for StrErr<'_> {}
 impl fmt::Display for StrErr<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
